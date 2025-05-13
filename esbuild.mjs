62825530d42334a164779c01adc6c9088fab2554
hypercu13e <imrandilm@gmail.com>
2025-05-13 23:06:24 +0200

    Prepare initial release
    I wonder if source maps will work, 'cause I haven't verified them using
    a local server. 🙃

--- esbuild.mjs
+++ esbuild.mjs
@@ -68,17 +68,25 @@ async function buildProjectDev() {
 }
 
 async function buildProjectProd() {
+	const { version } = packageManifest;
+	const homepageUrl = 'https://github.com/hypercu13e/bonus-viewer';
+	const releasesUrl = `${homepageUrl}/releases`;
+	const outFileName = 'bonus-viewer.user.js';
 	const userScriptBanner = dedent`
 		// ==UserScript==
 		// @name           Bonus Viewer
 		// @name:pl        Licznik bonusów
 		// @namespace      https://github.com/hypercu13e
-		// @version        ${packageManifest.version}
+		// @version        ${version}
 		// @description    Counts and displays item bonuses
 		// @description:pl Dodatek zliczający i wyświetlający bonusy przedmiotów
 		// @author         Hypercube
+		// @copyright      2025 Hypercube
+		// @homepage       ${homepageUrl}
 		// @icon           https://www.google.com/s2/favicons?sz=16&domain=margonem.pl
 		// @icon64         https://www.google.com/s2/favicons?sz=64&domain=margonem.pl
+		// @updateURL      ${releasesUrl}/latest/download/${outFileName}
+		// @downloadURL    ${releasesUrl}/latest/download/${outFileName}
 		// @match          https://*.margonem.pl/
 		// @match          https://*.margonem.com/
 		// @exclude        https://www.margonem.pl/
@@ -91,6 +99,21 @@ async function buildProjectProd() {
 		// @exclude        https://serwery.margonem.com/
 		// @grant          none
 		// ==/UserScript==
+
+		/* Copyright 2025 Hypercube
+		 *
+		 * Licensed under the Apache License, Version 2.0 (the "License");
+		 * you may not use this file except in compliance with the License.
+		 * You may obtain a copy of the License at
+		 *
+		 *    http://www.apache.org/licenses/LICENSE-2.0
+		 *
+		 * Unless required by applicable law or agreed to in writing, software
+		 * distributed under the License is distributed on an "AS IS" BASIS,
+		 * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+		 * See the License for the specific language governing permissions and
+		 * limitations under the License.
+		 */\n
 	`;
 
 	await esbuild.build({
@@ -104,9 +127,11 @@ async function buildProjectProd() {
 		},
 		format: 'iife',
 		logLevel: 'info',
-		outfile: path.join(OUT_DIR, 'bonus-viewer.js'),
 		minifySyntax: true,
+		outfile: path.join(OUT_DIR, outFileName),
 		platform: 'browser',
+		publicPath: `${releasesUrl}/download/${version}/`,
+		sourceRoot: `https://raw.githubusercontent.com/hypercu13e/bonus-viewer/refs/tags/${version}/build/${outFileName}`,
 		treeShaking: true,
 	});
 }
